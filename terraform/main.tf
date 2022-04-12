2821669a69e18d92221006929a094f1d0e5df102
Nathan Rhoda <nathan.rhoda@gmail.com>
2022-04-12 19:23:45 +0200

    terraform to create queue in aws

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = "${var.aws_region}"
 }
+
+resource "aws_kms_key" "this" {}
+
+resource "aws_sqs_queue" "nathan_queue" {
+  name                              = "nathan-queue"
+  kms_master_key_id                 = aws_kms_key.this.id
+  kms_data_key_reuse_period_seconds = 300
+}
+
