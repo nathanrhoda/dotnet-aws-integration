1b62cb0d3aff180408ff854294c9143d000d4737
Nathan Rhoda <nathan.rhoda@gmail.com>
2022-04-12 20:16:39 +0200

    Connection to AWS SQS

--- src/dotnet-aws-console-app/Program.cs
+++ src/dotnet-aws-console-app/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using dotnet_aws_sqs;
+
+string queueName = "https://sqs.af-south-1.amazonaws.com/479835631161/nathan-queue";
+
+await SqsService.Listen(queueName);
---
+++ src/dotnet-aws-integration/SqsService.cs
@@ -0,0 +1,63 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace dotnet_aws_sqs
+{
+    public static class SqsService
+    {
+        private const int MaxMessages = 1;
+        private const int WaitTime = 2;
+        public static async Task Listen(string queueName)
+        {
+            var sqsClient = new AmazonSQSClient();
+            Console.WriteLine($"Reading messages from queue\n  {queueName}");
+            Console.WriteLine("Press any key to stop. (Response might be slightly delayed.)");
+            do
+            {
+                var msg = await SqsService.GetMessage(sqsClient, queueName, WaitTime);
+                if (msg.Messages.Count != 0)
+                {
+                    if (SqsService.ProcessMessage(msg.Messages[0]))
+                        await SqsService.DeleteMessage(sqsClient, msg.Messages[0], queueName);
+                }
+            } while (!Console.KeyAvailable);
+        }
+        //
+        // Method to read a message from the given queue
+        // In this example, it gets one message at a time
+        private static async Task<ReceiveMessageResponse> GetMessage(
+          IAmazonSQS sqsClient, string qUrl, int waitTime = 0)
+        {
+            return await sqsClient.ReceiveMessageAsync(new ReceiveMessageRequest
+            {
+                QueueUrl = qUrl,
+                MaxNumberOfMessages = MaxMessages,
+                WaitTimeSeconds = waitTime
+                // (Could also request attributes, set visibility timeout, etc.)
+            });
+        }
+
+
+        //
+        // Method to process a message
+        // In this example, it simply prints the message
+        private static bool ProcessMessage(Message message)
+        {
+            Console.WriteLine($"\nMessage body of {message.MessageId}:");
+            Console.WriteLine($"{message.Body}");
+            return true;
+        }
+
+
+        //
+        // Method to delete a message from a queue
+        private static async Task DeleteMessage(
+          IAmazonSQS sqsClient, Message message, string qUrl)
+        {
+            Console.WriteLine($"\nDeleting message {message.MessageId} from queue...");
+            await sqsClient.DeleteMessageAsync(qUrl, message.ReceiptHandle);
+        }
+    }
+}
