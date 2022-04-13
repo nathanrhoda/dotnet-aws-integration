e6fedfdea4e98aec605de7a6b9e8628dfe7b2a00
Nathan Rhoda <nathan.rhoda@gmail.com>
2022-04-13 21:21:04 +0200

    added root resource id

--- terraform/modules/apigateway/output.tf
+++ terraform/modules/apigateway/output.tf
@@ -2,3 +2,8 @@ output "api_gateway_api_id" {
     description = "Api id for the Api Gateway"
     value       = aws_api_gateway_rest_api.NathanAPI.id
 }
+
+output "api_gateway_root_resource_id" {
+    description = "Api id for the Api Gateway"
+    value       = aws_api_gateway_rest_api.NathanAPI.root_resource_id
+}
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,8 @@
 output api_gateway_id{
   value = module.api-gateway.api_gateway_api_id
 }
+
+output "api_gateway_resource_id" {
+    description = "Api Root resource id for the Api Gateway"
+    value       = module.api-gateway.api_gateway_root_resource_id
+}
