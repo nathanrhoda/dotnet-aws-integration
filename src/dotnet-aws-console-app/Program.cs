1448280d5edb987e82bc6e6100762f5a0219c672
Nathan Rhoda <nathan.rhoda@gmail.com>
2022-04-17 15:52:22 +0200

    Updated console app to invoke api gateway and return result

--- src/dotnet-aws-console-app/Program.cs
+++ src/dotnet-aws-console-app/Program.cs
@@ -1,9 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Amazon.SQS;
-using Amazon.SQS.Model;
 using dotnet_aws_integration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 
-string queueName = "https://sqs.af-south-1.amazonaws.com/479835631161/nathan-queue";
+// Begin Startup Config
+var serviceCollection = new ServiceCollection();
+var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+var url = configuration["apigateway-url"];
+
+serviceCollection.AddHttpClient("ApiGateway", client =>
+{
+    client.BaseAddress = new System.Uri(url);
+});
+
+// End Startup Config
+
+
+// Api Gateway Integrations
+
+var client = new ApiGatewayClient(serviceCollection.BuildServiceProvider().GetRequiredService<IHttpClientFactory>());
+var response = await client.Post();
+var resp = await response.Content.ReadAsStringAsync(); 
+Console.WriteLine(JsonConvert.DeserializeObject(resp));
+
+// End Api Gateway Integrations
+
+
+// SQS Integrations
+string queueName = configuration["queue-name"];
 
 await SqsService.Listen(queueName);
 
+// End SQS Integrations
