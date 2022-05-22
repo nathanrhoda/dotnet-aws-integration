5d764a715734ff6bc6d2b406f7ff08fc098615ed
Nathan Rhoda <nathan.rhoda@gmail.com>
2022-05-22 15:42:44 +0200

    removed files no longer required

--- src/node-temporary-queue/temp-queue-client/index.js
+++
@@ -1,6 +0,0 @@
-const Client = require('./temp-queue-client.js')
-
-module.exports.sendMessage = async function(reqId, queueName, body){
-    var event = {requestId:reqId};
-    return Client.handler(event);
-};
